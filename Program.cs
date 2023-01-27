2e38e37f3350d68a90b83d0e0421126fd5099b8e
ARTEM <NickyWonder@ya.ru>
2023-01-27 22:23:57 +0300

    seminar 6 homework

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел строго больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 4
+
+// Для строки можно использовать данный метод string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries)(пользователь вводит в консоли числа через пробел и они формируют строку)
+
+// РЕШЕНИЕ:
+
+// Console.WriteLine("Введите числа через пробел: ");
+// int[] numbers = GetArrayFromString(Console.ReadLine()!);
+// Console.WriteLine($"Количество чисел > 0 = {PositiveCounts(numbers)}");
+// Console.WriteLine();
+
+// int[] GetArrayFromString(string stringArray){
+//     string[] numS = stringArray.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+//     int[] result = new int[numS.Length];
+//     for (int i=0; i<result.Length; i++){
+//         result[i] = int.Parse(numS[i]);
+//     }
+//     return result;
+// }
+
+// int PositiveCounts(int[] array){
+//     int count = 0;
+//     foreach (var item in array)
+//     {
+//         if(item>0) count++;
+//     }
+//     return count;
+//     }
+
+// ________________________________________________________________________________________________________________________________________
+// Задача 43: Написать программу, которая на вход принимает массив из любого количества элементов (не менее 6)в промежутке от 0 до 100, а на выходе выводит этот же массив, но отсортированный по возрастанию(от меньшего числа к большему).
+
+// РЕШЕНИЕ:
+
+// int[] array = NewArray(8);
+// Console.WriteLine($"[ {String.Join(", ", array)} ]");
+// SortArray(array);
+// Console.WriteLine($"[ {String.Join(", ", array)} ]");
+
+// int[] NewArray(int size){
+//     int[] array = new int[size];
+//     for(int i = 0; i < array.Length; i++){
+//         array[i] = new Random().Next(0, 100);
+//     }
+//     return array; 
+// }
+// void SortArray(int[] array){
+//     for(int i = 0; i < array.Length-1; i++){
+//         for (int j = i + 1; j < array.Length; j++){
+//             if(array[i] > array[j]){
+//                 int temp = array[i];
+//                 array[i] = array[j];
+//                 array[j] = temp;
+//             }
+//         }
+//     }
+// }
